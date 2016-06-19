bd3e2ffafc9190f2f434c7b2e40b73d42f2b5b69
Shea Norwood <sheanorwood@msn.comgit config --global user.email sheanorwood@msn.com>
2016-06-19 20:20:40 +0000

    Add devise gem

--- Gemfile
+++ Gemfile
@@ -2,11 +2,10 @@ source 'https://rubygems.org'
 
 
 gem 'simple_form'
-
 gem 'bootstrap-sass', '~> 3.3.5.1'
 gem 'sprockets', '2.11.0'
 gem 'sass-rails'
-
+gem 'devise', '>= 3.2.4'
 
 group :development, :test do
   gem 'rspec-rails', '~> 3.0'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Grammable::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.action_mailer.default_url_options = { host: 'grammable-shea-norwood.herokuapp.com' }
   # Code is not reloaded between requests.
   config.cache_classes = true
 

5090bd34fff6be4aabcc71ae179f4a10a6943799
Shea Norwood <sheanorwood@msn.comgit config --global user.email sheanorwood@msn.com>
2016-06-19 21:15:23 +0000

    Add factoryGirl

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'devise', '>= 3.2.4'
 
 group :development, :test do
   gem 'rspec-rails', '~> 3.0'
+  gem "factory_girl_rails", "~> 4.0"
 end
 
 gem 'rails_12factor', group: :production
--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -17,12 +17,7 @@ RSpec.describe GramsController, type: :controller do
 
 
         it "should successfully show the form" do
-            user = User.create(
-                email:          'fakeuser@example.com',
-                password:    'password',
-                password_confirmation:  'password'
-                )
-
+            user = FactoryGirl.create(:user)
             sign_in user
             get :new
             expect(response).to have_http_status(:success)
@@ -37,12 +32,8 @@ RSpec.describe GramsController, type: :controller do
 
 
         it "should successfully create a new gram in our database" do
-            user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword'
-        )
-        sign_in user
+            user = FactoryGirl.create(:user)
+            sign_in user
 
             post :create, gram: {message: 'Hello!'}
             expect(response).to redirect_to root_path
@@ -53,11 +44,7 @@ RSpec.describe GramsController, type: :controller do
           end
 
           it "should properly deal with validation errors" do
-            user = User.create(
-            email:                 'fakeuser@gmail.com',
-            password:              'secretPassword',
-            password_confirmation: 'secretPassword'
-              )
+            user = FactoryGirl.create(:user)
             sign_in user
 
             post :create, gram: {message: ' '}

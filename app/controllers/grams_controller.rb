be6bd649fe798dcc4d9d74981d9ed312e606486a
Shea Norwood <sheanorwood@msn.comgit config --global user.email sheanorwood@msn.com>
2016-06-23 01:59:22 +0000

    Secure edit & destroy

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,5 +1,5 @@
 class GramsController < ApplicationController
-before_action :authenticate_user!, only: [:new, :create]
+before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
 def index
 end
@@ -27,11 +27,17 @@ def show
 def edit
  @gram = Gram.find_by_id(params[:id])
  return render_not_found if @gram.blank?
+ if @gram.user != current_user
+    return render_not_found(:forbidden) if @gram.user != current_user
+  end
 end
 
 def update
   @gram = Gram.find_by_id(params[:id])
   return render_not_found if @gram.blank?
+  if @gram.user != current_user
+    return render_not_found(:forbidden) if @gram.user != current_user
+  end
 
   @gram.update_attributes(gram_params)
 
@@ -45,6 +51,10 @@ end
 def destroy
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
+    if @gram.user != current_user
+    return render_not_found(:forbidden) if @gram.user != current_user
+  end
+
     @gram.destroy
     redirect_to root_path
 end
@@ -56,8 +66,8 @@ def gram_params
     params.require(:gram).permit(:message)
 end
 
-def render_not_found
-    render text: 'Not Found :(', status: :not_found
+def render_not_found(status=:not_found)
+    render text: "#{status.to_s.titleize} :(", status: status
   end
 
 end
